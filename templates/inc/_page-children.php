43bd24bab0546ae9a65656a32525a67ef438985e
Rafał Olszewicz <rafalolszewicz@gmail.com>
2018-08-25 05:43:36 +0200

    update

--- templates/inc/_page-children.php
+++ templates/inc/_page-children.php
@@ -1,31 +1,31 @@
 <?php namespace ProcessWire;
 
 // Show Home page Children
-        echo pageChildren(pages(1),
-                [
-                    'limit'=> 12,
-                //  'random' => true
-            ]
-        );
+echo pageChildren(pages(1),
+    [
+        'limit'=> 12,
+      //  'random' => true
+    ]
+);
 
-    // Get Categories
-        echo catTag(pages()->get('/categories/'), 
-            [
-                // 'txt' => __('Categories'),
-                'limit' => 9,
-                'random' => true // Randomize Items
-            ] 
-        );
+// Get Categories
+echo catTag(page()->opt['cat_p'], 
+    [
+      // 'txt' => __('Categories'),
+        'limit' => 9,
+        'random' => true // Randomize Items
+    ] 
+);
 
-    // Get Tags     
-        echo catTag(pages()->get('/tags/'), 
-            [
-                // 'txt' => __('Tags'),
-                'limit' => 8,
-                'ul_cl' => 'grid', // Element <ul class='grid'
-                'li_cl' => 'col', // Element <li class='col'
-                'class' => 'button button-outline', // Element <a class='button button-outline',
-                'random' => true, // Randomize Items
-                'dis_count' => true // hide Count Down
-            ] 
+// Get Tags     
+echo catTag(page()->opt['tag_p'], 
+    [
+      // 'txt' => __('Tags'),
+        'limit' => 8,
+        'ul_cl' => 'grid', // Element <ul class='grid'
+        'li_cl' => 'col', // Element <li class='col'
+        'class' => 'button button-outline', // Element <a class='button button-outline',
+        'random' => true, // Randomize Items
+        'dis_count' => true // hide Count Down
+    ] 
 );
