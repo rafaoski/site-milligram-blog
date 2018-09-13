d82ccc17ea0d753250f70eefbb8aa7f5a83a3ca5
Rafał Olszewicz <rafalolszewicz@gmail.com>
2018-09-13 14:10:51 +0200

    update content

--- templates/contact.php
+++ templates/contact.php
@@ -9,19 +9,18 @@ $contactMail = page()->email;
 <div id='content-body'>
 
 <?php // Include contact form
-        wireIncludeFile("inc/_c-form",
-        [   'saveMessage' => true, // true or false
-            'contactPage' => page(), // Get Contact Page to save items pages('/contact/')
-            'contactItem' => 'contact-item', // Template to create item ( It must have a body field )
-            'mailTo' => $contactMail ?: 'user@gmail.com', // Send To Mail
-            'mailSubject' => page()->ts['mailSubject'], // Mail Subject
-        ]);
+wireIncludeFile("inc/_c-form",
+[   'saveMessage' => true, // true or false
+    'contactPage' => page(), // Get Contact Page to save items pages('/contact/')
+    'contactItem' => 'contact-item', // Template to create item ( It must have a body field )
+    'mailTo' => $contactMail ?: 'user@gmail.com', // Send To Mail
+    'mailSubject' => page()->ts['mailSubject'], // Mail Subject
+]);
 
 // Show basic Body field
 echo page()->body;?> 
 
 </div><!-- /#content-body -->
-
 
 <aside id="sidebar" pw-append>
 
@@ -30,29 +29,29 @@ echo page()->body;?>
 if($phoneNumber) {
 
 // Phone
-    echo icon('phone',
-      [
-        'txt' => ' ' . $phoneNumber . '<br>',
-        'url' =>  "tel:$phoneNumber",
-        'width' => 30,
-        'height' => 30,
-        'color' => '#9b4dca',
-        'stroke' => 2
-      ]);
-    
+  echo icon('phone',
+    [
+      'txt' => ' ' . $phoneNumber . '<br>',
+      'url' =>  "tel:$phoneNumber",
+      'width' => 30,
+      'height' => 30,
+      'color' => '#9b4dca',
+      'stroke' => 2
+    ]);
+
 }
 // Mail   
 if($contactMail) {
     
-    echo icon('mail',
-      [
-        'txt' => ' ' . $contactMail,
-        'url' =>  "mailto:$contactMail",
-        'width' => 30,
-        'height' => 30,
-        'color' => '#9b4dca',
-        'stroke' => 2
-      ]);
+  echo icon('mail',
+    [
+      'txt' => ' ' . $contactMail,
+      'url' =>  "mailto:$contactMail",
+      'width' => 30,
+      'height' => 30,
+      'color' => '#9b4dca',
+      'stroke' => 2
+    ]);
     
 }
 ?>
